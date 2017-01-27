23aa5dea29807fba1d3adb5171dbd1a8b041ac66
anuj1207 <anuj.saxena@knoldus.in>
2017-01-27 13:18:06 +0530

    Update MailVerifier.scala
    added invalid checker

--- MailVerifier.scala
+++ MailVerifier.scala
@@ -13,9 +13,14 @@ object MailVerifier {
 
   def main(args: Array[String]) {
     val mailVerifier = new MailVerifier()
-    val mailVerifier.pattern(user,domain)= "ramandeep.kaur@knoldus.in"
-    println(s"USER   :: $user")
-    println(s"DOMAIN :: $domain")
+    val input = "ramandeep.kaur@knoldus.in"
+    input match {
+      case mailVerifier.pattern(user,domain)=>{
+        println(s"USER   :: $user")
+        println(s"DOMAIN :: $domain")
+      }
+      case _ => println("Invalid Email")
+    }
   }
 
 }
